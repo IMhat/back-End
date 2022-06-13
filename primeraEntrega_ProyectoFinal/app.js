9e47074fd57ea65b87cf11ff4595f6900c69d35f
Mateo Mansilla <mateobariloche28@gmail.com>
2022-06-13 20:43:18 -0300

    Status404

--- primeraEntrega_ProyectoFinal/app.js
+++ primeraEntrega_ProyectoFinal/app.js
@@ -17,6 +17,12 @@ app.get('/', (req, res) =>{
 
 });
 
+app.use((req, res, next,) =>{
+    res.status(404).render("404");
+
+
+});
+
 
 app.listen(port, () =>{
     console.log(`Escuchando el puerto ${port}`)
